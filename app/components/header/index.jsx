623cef36b557bde922590884f66abb2e1c903094
Alexandra Spalato <alexaspalato@gmail.com>
2023-01-22 13:21:33 +0100

    Revert "Revert "framer motion""
    This reverts commit e555b892248f102db431cddeb477592ba3fd61ed.

--- app/components/header/index.jsx
+++ app/components/header/index.jsx
@@ -3,6 +3,7 @@ import TopMenu from "./TopMenu";
 import SlideMenu from "./SlideMenu";
 import Logo from "~/components/Logo";
 import Headroom from "react-headroom";
+import { motion } from "framer-motion";
 
 const Header = () => {
   const { headerNav, logo } = useLoaderData();
@@ -11,7 +12,18 @@ const Header = () => {
     // <Headroom>
     <header className="bg-black w-full">
       <div className="flex items-center center-container justify-between  py-5  md:py-10">
-        <Logo logo={logo} />
+        <motion.div
+          animate={{
+            x: 0,
+            opacity: 1,
+          }}
+          initial={{ x: -100, opacity: 0 }}
+          transition={{
+            duration: 1.5,
+          }}
+        >
+          <Logo logo={logo} />
+        </motion.div>
         <TopMenu navItems={headerNav} className="hidden lg:flex" />
         <SlideMenu navItems={headerNav} className="lg:hidden" />
       </div>
