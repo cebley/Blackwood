b26944221a7363ac58fbe9ecfc92b31498f6e79b
Alexandra Spalato <alexaspalato@gmail.com>
2023-01-28 10:24:46 +0100

    lights

--- app/components/storyblok/Hero.jsx
+++ app/components/storyblok/Hero.jsx
@@ -20,34 +20,22 @@ const Hero = ({ blok }) => {
         initial="hidden"
         whileInView="visible"
         transition={{ duration: 1.5, delay: 0.5 }}
-        className="center-container max-w-[986px] pt-[100px] pb-[90px] md:pt-[100px] md:pb-[180px] lg:pb-[300px]"
+        className="center-container max-w-[986px] pt-[100px] pb-[50px] md:pt-[100px] "
       >
         <h1>{headline}</h1>
         <div className="text-white ">{render(intro)}</div>
-
-        {image && (
-          <div className="flex justify-center mt-[143px]">
-            <img src={image.filename} alt="" />
-          </div>
-        )}
-        {bottom_text && (
-          <div
-            dangerouslySetInnerHTML={{ __html: bottom_text }}
-            className="text-white"
-          />
-        )}
       </motion.div>
 
       {!noOblique && (
-        <TriangleMask className="absolute object-cover h-[300px] w-full bottom-0 md:bottom-[-20px] lg:bottom-[-70px]" />
+        <TriangleMask className="absolute object-cover h-[400px] w-full bottom-0 " />
       )}
       {!noLights && (
         <motion.div
           variants={slideInUp}
           initial="hidden"
-          whileInView={{ y: 0, opacity: 0.7 }}
+          whileInView={{ y: -30, opacity: 0.7 }}
           transition={{ duration: 1.5, delay: 2 }}
-          className="absolute w-full h-[300px] pointer-events-none bottom-5 -md:bottom-5 -lg:bottom-[70px] z-0"
+          className="page-lights "
         >
           <PageLights />
         </motion.div>
