2b1330ca48f04a62fb065cab25425f3df8bf3f78
Alexandra Spalato <alexaspalato@gmail.com>
2023-01-28 11:28:30 +0100

    fix triangle

--- app/components/storyblok/Hero.jsx
+++ app/components/storyblok/Hero.jsx
@@ -6,8 +6,7 @@ import TriangleMask from "~/inline-svgs/TriangleMask";
 import PageLights from "~/inline-svgs/PageLights";
 
 const Hero = ({ blok }) => {
-  const { _uid, headline, intro, image, bottom_text, noLights, noOblique } =
-    blok;
+  const { _uid, headline, intro, smaller, noLights, noOblique } = blok;
   //TODO: fix triangle and lights on small and very big screens
   return (
     <div
@@ -20,22 +19,22 @@ const Hero = ({ blok }) => {
         initial="hidden"
         whileInView="visible"
         transition={{ duration: 1.5, delay: 0.5 }}
-        className="center-container max-w-[986px] pt-[100px] pb-[50px] md:pt-[100px] "
+        className={`container  ${
+          smaller ? "!max-w-[710px]" : "!max-w-[986px]"
+        } mx-auto pt-[100px] pb-[180px] md:pb-[180px] md:pt-[100px]`}
       >
         <h1>{headline}</h1>
         <div className="text-white ">{render(intro)}</div>
       </motion.div>
 
-      {!noOblique && (
-        <TriangleMask className="absolute object-cover h-[400px] w-full bottom-0 " />
-      )}
+      {!noOblique && <div className="triangle"></div>}
       {!noLights && (
         <motion.div
           variants={slideInUp}
           initial="hidden"
           whileInView={{ y: -30, opacity: 0.7 }}
           transition={{ duration: 1.5, delay: 2 }}
-          className="page-lights "
+          className="page-lights absolute w-full h-[300px] pointer-events-none bottom-5 -md:bottom-5 lg:bottom-[-70px] z-0 "
         >
           <PageLights />
         </motion.div>
