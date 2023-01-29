2bd7d714d92f5e4183620ccaaaaf7b6d068aaa65
Alexandra Spalato <alexaspalato@gmail.com>
2023-01-29 14:27:28 +0100

    blackwood logo and columns

--- app/components/storyblok/Technologies.jsx
+++ app/components/storyblok/Technologies.jsx
@@ -1,7 +1,7 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
 const Technologies = ({ blok }) => {
-  const { _uid, logos, columns } = blok;
+  const { _uid, logos, columns, blackwoodLogo } = blok;
   console.log("logos", logos);
   return (
     <div
@@ -12,6 +12,18 @@ const Technologies = ({ blok }) => {
       {logos?.map((nestedBlok) => (
         <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
+      {blackwoodLogo && (
+        <div className="card flipCard blackwood">
+          <div className="flipper">
+            <div className="front boxes flex">
+              <img src="/images/logo-black.svg" alt="logo" />
+            </div>
+            <div className="back boxes flex">
+              <img src="/images/logo-small.svg" alt="logo" />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
