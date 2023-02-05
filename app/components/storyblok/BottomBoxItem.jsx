5f6629bcf1d080bb1c7e4448afcaa9f4c7e84794
Alexandra Spalato <alexaspalato@gmail.com>
2023-02-05 17:31:37 +0100

    bottom boxes

--- app/components/storyblok/BottomBoxItem.jsx
+++ app/components/storyblok/BottomBoxItem.jsx
@@ -7,17 +7,19 @@ const BottomBoxItem = ({ blok }) => {
     <div
       {...storyblokEditable(blok)}
       key={blok._uid}
-      className=" relative grow-1 shrink-1 basis-auto lg:flex-1 -mb-16 group"
+      className="relative -mb-16 grow-1 shrink-1 basis-auto lg:flex-1 group"
     >
       <Link to={link}>
-        <img
-          src={`${image?.filename}`}
-          alt={image?.alt}
-          className="object-cover min-h-[363px]"
-        />
+        <div>
+          <img
+            src={`${image?.filename}`}
+            alt={image?.alt}
+            className="object-cover min-h-[363px] w-full"
+          />
+        </div>
         <div className="box-content max-w-[310px] pt-[200px] pb-[30px] px-[35px] text-xs leading-[1.4] absolute inset-0 [&>a]:text-white">
-          <h3 className="uppercase text-white mb-3">{title}</h3>
-          <div className="box-over opacity-0 group-hover:opacity-100 transition duration-500">
+          <h3 className="mb-3 text-white uppercase">{title}</h3>
+          <div className="transition duration-500 opacity-0 box-over group-hover:opacity-100">
             <div className="text-white font-bold max-w-[240px]">
               {description}
             </div>
