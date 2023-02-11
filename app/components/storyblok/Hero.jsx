a7806723df68c250061ba1f1884644a958bf3a78
Alexa spalato <alexaspalato@gmail.com>
2023-02-11 10:23:26 +0100

    hero fixed

--- app/components/storyblok/Hero.jsx
+++ app/components/storyblok/Hero.jsx
@@ -7,39 +7,34 @@ import Breadcrumbs from "~/components/Breadcrumbs";
 
 const Hero = ({ blok }) => {
   const { _uid, headline, intro, smaller, noLights, noOblique } = blok;
-  //TODO: fix triangle and lights on small and very big screens
-  return (
-    <div
-      {...storyblokEditable(blok)}
-      key={_uid}
-      className="relative overflow-hidden bg-black"
-    >
-      <Breadcrumbs location="hero" />
-      <div
-        variants={slideInUp}
-        initial="hidden"
-        whileInView="visible"
-        transition={{ duration: 1.5, delay: 0.5 }}
-        className={`container  ${
-          smaller ? "!max-w-[710px]" : "!max-w-[986px]"
-        } mx-auto pt-[100px] pb-[180px] md:pb-[180px] md:pt-[100px]`}
-      >
-        <h1>{headline}</h1>
-        <div className="max-w-full prose text-white ">{render(intro)}</div>
-      </div>
 
-      {!noOblique && <div className="triangle"></div>}
+  return (
+    <div {...storyblokEditable(blok)} key={_uid} className="hero">
       {!noLights && (
-        <div
-          variants={slideInUp}
-          initial="hidden"
-          whileInView={{ y: -30, opacity: 0.7 }}
-          transition={{ duration: 1.5, delay: 2 }}
-          className="page-lights absolute w-full h-[300px] pointer-events-none bottom-5 -md:bottom-5 lg:bottom-[-70px] z-0 "
-        >
+        // <motion.div
+        //   variants={slideInUp}
+        //   initial="hidden"
+        //   whileInView={{ y: -30, opacity: 0.7 }}
+        //   transition={{ duration: 1.5, delay: 2 }}
+        //   className="page-lights absolute w-full h-[300px] pointer-events-none bottom-5 -md:bottom-5 lg:bottom-[-70px] z-0 "
+        // >
+        <div className="lights">
           <PageLights />
         </div>
+        // </motion.div>
       )}
+      <div className="hero-container">
+        <div className="page-intro">
+          <Breadcrumbs location="hero" />
+          <div className="intro-container">
+            <h1>{headline}</h1>
+            <div className="max-w-full prose text-white page-intro ">
+              {render(intro)}
+            </div>
+          </div>
+        </div>
+      </div>
+      {!noOblique && <div className="triangle"></div>}
     </div>
   );
 };
