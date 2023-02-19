365f14278327b5d4043fca78cc37bc6d80e288d5
Alexa spalato <alexaspalato@gmail.com>
2023-02-19 08:24:21 +0100

    fox bottom boxes

--- app/components/storyblok/BottomBoxItem.jsx
+++ app/components/storyblok/BottomBoxItem.jsx
@@ -13,14 +13,15 @@ const BottomBoxItem = ({ blok }) => {
         <div className="absolute inset-0 w-full h-full transition duration-500 opacity-0 box-overlay group-hover:opacity-50 bg-primary"></div>
         <div className="w-full">
           <img
-            src={`${image?.filename}/m/`}
+            src={`${image?.filename}/m/600x363/smart`}
             alt={image?.alt}
             className="object-cover h-[363px] w-full"
           />
         </div>
-        <div className="box-content max-w-[310px] pt-[200px] pb-[30px] px-[35px] text-xs leading-[1.4] absolute inset-0 [&>a]:text-white">
+        <div className="box-content max-w-[310px] pt-[120px] lg:pt-[200px] pb-[30px] px-[35px] text-xs leading-[1.4] absolute inset-0 [&>a]:text-white ">
           <h3 className="mb-3 text-white uppercase">{title}</h3>
-          <div className="transition duration-500 opacity-0 box-over group-hover:opacity-100">
+
+          <div className="flex items-center justify-between transition duration-500 opacity-0 box-over group-hover:opacity-100">
             <div className="text-white font-bold max-w-[240px]">
               {description}
             </div>
@@ -28,7 +29,7 @@ const BottomBoxItem = ({ blok }) => {
             <img
               src="/images/logo-small-white.svg"
               alt="logo"
-              className="absolute right-[35px] bottom-[60px]"
+              className="transition duration-500 opacity-0 box-over group-hover:opacity-100 "
             />
           </div>
         </div>
