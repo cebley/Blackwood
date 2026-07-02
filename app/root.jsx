ec9307663494c73f1849218999b849148221879f
alexandra spalato <alexaspalato@gmail.com>
2026-07-02 10:15:53 -0300

    favicon

--- app/root.jsx
+++ app/root.jsx
@@ -139,6 +139,9 @@ const Document = ({ children }) => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <Meta />
         <Links />
       </head>
